e421d3556ebef33f08b2702f8e13c4ae28615a46
Aesl Kim <aesl.kim@kellogg.northwestern.edu>
2023-04-20 01:06:20 +0000

    second commit- class

--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -13,10 +13,28 @@ p we_have_to_go_back
 mixed_array = ["tacos", 3, true]
 p mixed_array
 
+shopping_lists = [["diapers", "wipes", 'toys'],['coffee', 'bear']]
+p shopping_lists
 
 # Accessing the array
 
+puts foods[0]
+puts foods[1]
+puts foods[2]
+p foods[3]
+puts foods[-1]
+puts foods[-2]
+puts foods[-4]
+
+puts shopping_lists[1][0]
+puts shopping_lists[0][0]
+
 # Add to the array
+
+foods.push("salad")
+foods.append("salad")
+foods << "fries"
+p foods
 
 # There are lots of fun things you can do with arrays:
 # https://ruby-doc.org/core-2.7.0/Array.html
