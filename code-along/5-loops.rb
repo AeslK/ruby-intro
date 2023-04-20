a353117d35e07f9823138be2d28a30ef16c590d9
Aesl Kim <aesl.kim@kellogg.northwestern.edu>
2023-04-20 02:31:25 +0000

    fifth commit - class

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -7,5 +7,35 @@
 #   puts "tacos!"
 # end
 
+# index = 0
+# loop do 
+#     if index == 5
+#         break
+#     end
+
+#     puts "tacos!"
+#     index = index + 1
+# end
+
+
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+ingredients = ["carnitas", "carne asada", "pollo", "pescado", "mushroom"]
+
+# index = 0
+
+# loop do 
+#     if index == ingredients.size
+#         break
+#     end
+
+#     taco = ingredients[index]
+
+#     puts "#{taco} tacos!"
+#     index = index + 1
+# end
+
+for aesl in ingredients
+    # do something
+    puts "#{aesl} tacos!"
+
+end
