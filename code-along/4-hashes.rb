44b2a6e7c2c3a29e66b06db0693378a72af9d9da
Aesl Kim <aesl.kim@kellogg.northwestern.edu>
2023-04-20 01:33:05 +0000

    third commit- class

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -1,9 +1,25 @@
 # To run this code, be sure your current working directory
 # is the same as where this file is located and then run:
 # ruby 4-hashes.rb
+# hash rocket operator (=>)
 
 # Hashes are lists of *key-value pairs*
 
+profile = {
+    "name" => "Ben",
+    "location" => {"city" => "Chicago", "state" => "O:"},
+    "timeline" => [
+        {"status" => "Teaching ENTR 451", "occuring_at" => "6:30 pm"},
+        {"status" => "Still taching...", "occuring_at" => "7:30 pm"}
+    ]
+}
+
+puts profile
+
 # Accessing data from the hash
+puts profile["name"]
+puts profile["location"]["city"]
+
 
 # More Complex Hashes
+puts profile ["timeline"][-1]["occuring_at"]
